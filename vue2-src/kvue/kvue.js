636a2acadf6312018a10d96ea5e10a4855f60c0d
guochao <463561248@qq.com>
2021-07-06 22:03:42 +0800

    简单了解 虚拟dom

--- vue2-src/kvue/kvue.js
+++ vue2-src/kvue/kvue.js
@@ -84,20 +84,100 @@ class KVue {
     this.$el = document.querySelector(el);
     // 创建一个 updataComponent 方法
     const updateComponent = () => {
-      // 执行 一个 _render
-      const dom = this.$options.render.call(this)
-      const parent = this.$el.parentElement;
-      parent.insertBefore(dom, this.$el.nextSibling)
-      parent.removeChild(this.$el);
-      this.$el = dom
-      // 执行一个 _update 
+      
+      // const dom = this.$options.render.call(this)
+      // const parent = this.$el.parentElement;
+      // parent.insertBefore(dom, this.$el.nextSibling)
+      // parent.removeChild(this.$el);
+      // this.$el = dom
+
+      const { render } = this.$options
+      // 执行 一个 render
+      const vnode = render.call(this, this.$createElement)
+      // 执行一个 _update
+      this._update(vnode)
     }
 
     // 创建 一个 Watcher 对象
     new Watcher(this, updateComponent)
   }
-}
 
+  // 创建虚拟dom
+  $createElement(tag, attr, children, elm) {
+    return {tag, attr, children, elm}
+  }
+
+  _update(vnode) {
+    // 上一次 虚拟 vnode
+    const prevVnode = this._vnode
+
+    if(!prevVnode){
+      // 初始化
+      this.__patch__(this.$el, vnode)
+    }else{
+      // 更新
+      this.__patch__(prevVnode, vnode)
+    }
+  }
+
+  __patch__(oldVnode, vnode){
+    // oldVnode 是真实 DOM
+    this.$$patch(oldVnode, vnode) 
+  }
+  $$patch(oldVnode, vnode) {
+    // 源码中是 通过 createPatchFunction 创建的 patch 方法， 这边只是模拟
+    // 平台相关
+    
+    // 1. 如果新的 vnode 没有 但是 oldVnode  存在  就是删除操作
+
+    // 2. 如果 旧的不存在 但是新的存在 就是 新增操作
+
+    // 3. 正常存在
+    // 判断 oldVnode 是否是原生dom
+    // const isRealElement = isDef(oldVnode.nodeType)
+    const isRealElement = oldVnode.nodeType
+    // if (!isRealElement && sameVnode(oldVnode, vnode)) {
+
+    // }else{
+          // 如果是真实 dom
+          if(isRealElement){
+            // oldVnode 转换成虚拟DOM
+            oldVnode = emptyNodeAt(oldVnode, this)
+          }
+          const oldElm = oldVnode.elm
+          const parentElm =  oldElm.parentElement
+          this.createElm.call(this, vnode, parentElm, oldElm.nextSibling)
+          parentElm.removeChild(oldElm);
+          this._vnode = vnode
+    // }
+  }
+
+  createElm(vnode, parentElm, refElm) {
+    const children = vnode.children
+    const tag = vnode.tag
+    if(tag){
+      const tagdom = document.createElement(tag)
+      vnode.elm = tagdom
+      this.createChildren(vnode, children)
+      // inser
+      parentElm.insertBefore(tagdom, refElm)
+    }
+    console.log('???xx')
+  }
+  createChildren(vnode, children){
+    if (Array.isArray(children)) {
+      for (let i = 0; i < children.length; ++i) {
+        this.createElm.call(this, children[i], vnode.elm, null)
+      }
+    }else{
+      vnode.elm.textContent = children 
+      // el.textContent = vnode.children;
+    }
+  }
+}
+function emptyNodeAt(el, vm) {
+  return vm.$createElement(el.tag, {}, [], el)
+}
 // 监听器： 页面依赖收集
 class Watcher {
   constructor(vm, fn) {
