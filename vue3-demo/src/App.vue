8bcf6b0b2117c536f3a8318dc17c177f42265f3e
guochao <463561248@qq.com>
2021-07-13 10:11:20 +0800

    vue3-demo

---
+++ vue3-demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  {{ name }}
+  <button>这是一个按钮</button>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  // data() {
+  //   return {
+  //     name: 'saber'
+  //   }
+  // },
+  setup(){
+    return {
+      name: 'kurama'
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
